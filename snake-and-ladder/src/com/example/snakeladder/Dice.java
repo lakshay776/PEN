ba29285019be62d57b2b79efe814cc25f85b3d50
Lakshay Jagga <jagga.lakshay776@gmail.com>
2026-03-24 01:52:31 +0530

    Delete snake-and-ladder directory

--- snake-and-ladder/src/com/example/snakeladder/Dice.java
+++
@@ -1,9 +0,0 @@
-package com.example.snakeladder;
-
-/**
- * Strategy interface for rolling a dice.
- */
-public interface Dice{
-    int roll();
-    int faces();
-}
--- snake-and-ladder/src/com/example/snakeladder/DifficultyLevel.java
+++
@@ -1,6 +0,0 @@
-package com.example.snakeladder;
-
-public enum DifficultyLevel {
-    EASY,
-    HARD
-}
